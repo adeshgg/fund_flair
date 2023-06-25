161af898107bd3d137b9d5848535de6cf3b7bb91
Adesh Gupta <adesh02092000@gmail.com>
2023-06-25 14:39:24 +0530

    Added basic styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,25 +5,33 @@ export const metadata = {
 
 export default function About() {
   return (
-    <main>
-      <h1>Welcome to Fund Flair!</h1>
+    <main className='p-10'>
+      <h1 className='m-10 ml-0 font-extrabold text-2xl md:text-4xl lg:text-6xl'>
+        Welcome to Fund Flair!
+      </h1>
 
-      <h2>Why Us?</h2>
-      <ul>
+      <h2 className='m-4 ml-0 font-bold text-1xl md:text-2xl lg:text-4xl'>
+        Why Us?
+        <hr />
+      </h2>
+      <ul className='ml-4 list-disc'>
         <li>Easy-to-use Tools</li>
         <li>Fundraising Success</li>
         <li>Social Media Integration</li>
         <li>Flexible Pricing Options</li>
       </ul>
 
-      <h2>How Does Crowdfunding Work?</h2>
+      <h2 className='m-4 ml-0 font-bold text-1xl md:text-2xl lg:text-4xl'>
+        How Does Crowdfunding Work?
+        <hr />
+      </h2>
       <p>
         When you join our crowdfunding platform, you'll need to create a profile
         and set up your campaign. In your campaign, you'll share your story,
         explain why you're raising funds, and set a fundraising goal. Once your
         campaign is live, you can start promoting it to attract backers.
       </p>
-
+      <br />
       <p>
         Depending on the type of crowdfunding site you choose, you may have a
         specific campaign duration, such as 30 days or longer. During this
@@ -32,14 +40,18 @@ export default function About() {
         your fundraising account upon registration.
       </p>
 
-      <h2>Our Commitment</h2>
+      <h2 className='m-4 ml-0 font-bold text-1xl md:text-2xl lg:text-4xl'>
+        Our Commitment
+      </h2>
       <p>
         We are committed to providing a reliable and secure crowdfunding
         platform for both fundraisers and backers. We prioritize the success and
         satisfaction of our users, and we strive to create a positive and
         supportive environment for crowdfunding campaigns.
       </p>
-      <h3>Join us today and start turning your ideas into reality!</h3>
+      <h3 className='m-4 ml-0 font-bold text-1xl lg:text-2xl'>
+        Join us today and start turning your ideas into reality!
+      </h3>
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <section className='m-10 mt-20'>
-        <h1 className='font-extrabold text-4xl'>Fund Flair</h1>
-        <p className='font-extrabold text-4xl bg-gradient-to-r from-purple-900 via-indigo-700 to-pink-700 bg-clip-text text-transparent'>
+      <section className='m-10 mt-20 space-y-5 font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>
+        <h1>Fund Flair</h1>
+        <p className='bg-gradient-to-r from-purple-900 via-indigo-700 to-pink-700 bg-clip-text text-transparent'>
           Support. Discover. Empower.
         </p>
       </section>
