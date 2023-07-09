699b62af870a0ebcd31edce5c9fd162ebb65a0f8
Adesh Gupta <adesh02092000@gmail.com>
2023-07-09 19:09:44 +0530

    Removed all the logs

--- app/api/checkout/[id]/route.ts
+++ app/api/checkout/[id]/route.ts
@@ -3,9 +3,11 @@ import { NextResponse, NextRequest } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  //@ts-ignore
   apiVersion: '2020-08-27',
 })
 
+// @ts-ignore
 export async function GET(request, context) {
   const id = context.params.id
 
